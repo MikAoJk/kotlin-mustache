82a5e1fd3a222e9280464ccb76c1381b3011afbe
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2021-08-26 08:05:45 +0200

    Updated kotlin to 1.5.30

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ val logstashEncoderVersion = "5.1"
 
 plugins {
     java
-    kotlin("jvm") version "1.4.30"
+    kotlin("jvm") version "1.5.30"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
