8121a1627043701835aab3bea3fee853514b6bcd
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2022-05-25 11:31:45 +0200

    Fixed bug

--- src/main/kotlin/no/kartveit/application/ApplicationServer.kt
+++ src/main/kotlin/no/kartveit/application/ApplicationServer.kt
@@ -14,6 +14,6 @@ class ApplicationServer(
     }
 
     fun start() {
-        applicationServer.start(false)
+        applicationServer.start(true)
     }
 }
--- src/main/kotlin/no/kartveit/service/LoginService.kt
+++ src/main/kotlin/no/kartveit/service/LoginService.kt
@@ -1,8 +1,8 @@
 package no.kartveit.service
 
 class LoginService {
-    fun validateUser(userName: String, password: String): Boolean {
+    fun validateUser(username: String, password: String): Boolean {
         // TODO change to call a database with to validate username and password is correct
-        return userName.equals("joakim", ignoreCase = true) && password.equals("1234", ignoreCase = false)
+        return username.equals("joakim", ignoreCase = true) && password.equals("1234", ignoreCase = false)
     }
 }
